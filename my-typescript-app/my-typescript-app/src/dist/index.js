f997e83266ab2911a5e3b6e88bae133f4388550b
Adedunmola Shonekan <a.shonekan96@gmail.com>
2021-05-20 21:01:19 +0100

    Some stuff was done....

---
+++ my-typescript-app/my-typescript-app/src/dist/index.js
@@ -0,0 +1,8 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './sass/index.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render(_jsx(React.StrictMode, { children: _jsx(App, {}, void 0) }, void 0), document.getElementById('root'));
+reportWebVitals();
